a93a7f250fffbcceeefa0c872f0914ed8ac1c0e6
Adam Scott <adam.d.scott@gmail.com>
2013-05-17 20:41:56 +1000

    Shiny. Well, shiny-er.

--- spec/index.haml_spec.rb
+++ spec/index.haml_spec.rb
@@ -6,7 +6,7 @@ describe 'index.haml' do
 
   let(:copy) { 'Some copy' }
   subject { Haml::Engine.new(File.read('site/index.haml')).render(OpenStruct.new(:quote => {:copy => copy})) }
-	
-  it { should have_tag('p') { with_text copy } }
-	
+
+  it { should have_tag('blockquote') { with_text copy } }
+
 end
