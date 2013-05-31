f1d32b13b6a06532fc8215401df464d2badb0ed2
Adam Scott <adam.d.scott@gmail.com>
2013-05-31 14:23:27 +1000

    Now caching static assets.

--- app.rb
+++ app.rb
@@ -16,6 +16,10 @@ def find_quote(hash_string)
   QUOTES.find { |quote| hash_string == quote.hash_string }
 end
 
+def far_future
+  Time.now + 60*60*24*356*3
+end
+
 set :json_encoder, JSON
 
 get '/' do
@@ -37,5 +41,20 @@ get '/:quote_hash_string' do |quote_hash_string|
 end
 
 get '/stylesheets/:stylesheet.css' do |stylesheet|
+  expires far_future, :public, :must_revalidate
   scss :"stylesheets/#{stylesheet}"
+end
+
+helpers do
+  def versioned_static(path)
+    "#{path}?#{File.mtime(File.join(Sinatra::Application.public_folder, path))}"
+  end
+
+  def versioned_stylesheet(stylesheet)
+    "/stylesheets/#{stylesheet}.css?#{File.mtime(File.join(Sinatra::Application.views, 'stylesheets', "#{stylesheet}.scss")).to_i.to_s}"
+  end
+
+  def versioned_javascript(js)
+    versioned_static("/javascripts/#{js}.js")
+  end
 end
